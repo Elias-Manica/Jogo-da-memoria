3a012acf3b3c80427ab4f107c5873c7c5fae17e8
Elias <eliasmanica@poli.ufrj.br>
2022-06-16 15:50:42 -0300

    adicionando funcionalidade de escolher qtd de cartas

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let contador = false;
+let numCartas = iniciarCartas();
+adicionarCartas();
 
+function iniciarCartas() {
+  while (contador !== true) {
+    let resposta = prompt("Com quantas cartas você quer jogar?");
+    let valor = resposta % 2;
+    if (
+      Number(resposta) !== 0 &&
+      Number(resposta) !== 2 &&
+      Number(resposta) <= 14 &&
+      valor === 0
+    ) {
+      contador = true;
+      return resposta;
+    }
+  }
+}
+
+function adicionarCartas() {
+  for (let i = 0; i < numCartas; i++) {
+    const qtdCartas = document.querySelector(".caixaComCartas");
+    const addCartas = `
+        <div class="carta"></div>
+    `;
+    qtdCartas.innerHTML += addCartas;
+  }
+}
